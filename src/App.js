7a8f3d9d8a9e67baef0d375644aa34ae554a7523
Zechariah-dev <omoladesunday2017@gmail.com>
2021-08-04 13:25:48 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+//pages
+const Home = React.lazy(() => import("./components/Home"));
+const Login = React.lazy(() => import("./components/Signup"));
+const Signup = React.lazy(() => import("./components/Login"))
+
+function App() {
+  return (
+    <React.Suspense fallback={<h1>Loading</h1>}>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login}/>
+          <Route path="/signup" component={Signup}/>
+        </Switch>
+      </React.Fragment>
+    </React.Suspense>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@material-ui/core/styles";
+import "./index.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#FF856D",
+      main: "#FF6246",
+    },
+    secondary: {
+      main: "#073567",
+    },
+    text: {
+      primary: "#455262",
+      secondary: "#000E1F",
+    },
+  },
+});
+
+const customTheme = responsiveFontSizes(theme);
+
+ReactDOM.render(
+  <ThemeProvider theme={customTheme}>
+    <Router>
+      <App />
+    </Router>
+  </ThemeProvider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
