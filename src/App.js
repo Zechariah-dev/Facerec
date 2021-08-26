1f872f5fc48ab484e5cde83e0ba2edaacee95a72
Waris Oloyede <oloyedewaris@gmail.com>
2021-08-26 23:47:19 +0100

    Waris just initialize

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-
 //pages
 const Home = React.lazy(() => import("./components/Home"));
-const Login = React.lazy(() => import("./components/Register"));
-const Signup = React.lazy(() => import("./components/Login"));
+const Signup = React.lazy(() => import("./components/Signup"));
+const Login = React.lazy(() => import("./components/Login"))
 
 function App() {
   return (
     <React.Suspense fallback={<h1>Loading</h1>}>
       <React.Fragment>
-        <Navbar/>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login}/>
+          <Route path="/signup" component={Signup}/>
         </Switch>
       </React.Fragment>
     </React.Suspense>
