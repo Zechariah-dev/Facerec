126c7ef91fd385bd24d5fc8a94f53dfb36375f5e
Omolade Sunday <38852467+Zechariah-dev@users.noreply.github.com>
2021-08-27 07:31:45 -0700

    chores: fixed deployment error

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
-import Grid from "@material-ui/core/grid";
-import Button from "@material-ui/core/button";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import Webcam from "react-webcam"
 
 // Navbar
