2744b954586faa1647e3c656b694a49098888643
Zechariah-dev <omoladesunday2017@gmail.com>
2021-11-17 12:52:15 -0800

    testing...

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,7 +5,7 @@ import Grid from '@material-ui/core/Grid';
 import * as Yup from 'yup';
 
 // useHistory for redirection
-import { useHistory } from "react-router-dom"
+import { useHistory } from 'react-router-dom';
 
 //images
 import onboardingImage from '../../assets/images/onboarding.png';
@@ -18,114 +18,106 @@ import SubmitButton from '../Forms/SubmitButton';
 import Navbar from '../Navbar';
 
 const validationSchema = Yup.object().shape({
-	email: Yup.string()
-		.required()
-		.email()
-		.matches(/^[a-zA-Z]+@student\.oauife\.edu\.ng$/)
-		.label('Email'),
-	surname: Yup.string().required().label('Surname'),
-	matricNumber: Yup.string()
-		.required()
-		.matches(/^[a-zA-Z]{3}\/[0-9]{4}\/[0-9]{3}$/)
-		.label('Matric number'),
-	// password: Yup.string().required().min(6).label('Password'),
+  email: Yup.string()
+    .required()
+    .email()
+    .matches(/^[a-zA-Z]+@student\.oauife\.edu\.ng$/)
+    .label('Email'),
+  surname: Yup.string().required().label('Surname'),
+  matricNumber: Yup.string()
+    .required()
+    .matches(/^[a-zA-Z]{3}\/[0-9]{4}\/[0-9]{3}$/)
+    .label('Matric number'),
+  // password: Yup.string().required().min(6).label('Password'),
 });
 
 const useStyles = makeStyles((theme) => ({
-	column: {
-		[theme.breakpoints.up('tablet')]: {
-			flex: '50%',
-		},
-	},
-	form: {
-		paddingTop: '1rem',
-	},
-	formContainer: {
-		flexDirection: 'column',
-		margin: 0,
-		maxWidth: '100%',
-	},
-	heading: {
-		fontSize: '1.5rem',
-		[theme.breakpoints.up('md')]: {
-			fontSize: '2.5rem',
-		},
-	},
-	onboarding: {
-		display: 'none',
-		alignItems: 'center',
-		[theme.breakpoints.up('tablet')]: {
-			display: 'flex',
-		},
-	},
-	root: {
-		background: `url(${backgroundImage}) no-repeat`,
-		minHeight: '100vh',
-	},
-	textLight: {
-		color: theme.palette.text.primary,
-		fontSize: '0.9rem',
-		[theme.breakpoints.up('tablet')]: {
-			fontSize: '1rem',
-		},
-	},
+  column: {
+    [theme.breakpoints.up('tablet')]: {
+      flex: '50%',
+    },
+  },
+  form: {
+    paddingTop: '1rem',
+  },
+  formContainer: {
+    flexDirection: 'column',
+    margin: 0,
+    maxWidth: '100%',
+  },
+  heading: {
+    fontSize: '1.5rem',
+    [theme.breakpoints.up('md')]: {
+      fontSize: '2.5rem',
+    },
+  },
+  onboarding: {
+    display: 'none',
+    alignItems: 'center',
+    [theme.breakpoints.up('tablet')]: {
+      display: 'flex',
+    },
+  },
+  root: {
+    background: `url(${backgroundImage}) no-repeat`,
+    minHeight: '100vh',
+  },
+  textLight: {
+    color: theme.palette.text.primary,
+    fontSize: '0.9rem',
+    [theme.breakpoints.up('tablet')]: {
+      fontSize: '1rem',
+    },
+  },
 }));
 
 export default function Login() {
-	// call useHistory hook
-	const history = useHistory();
+  // call useHistory hook
+  const history = useHistory();
 
-	const initialValues = {
-		email: '',
-		surname: '',
-		matricNumber: '',
-		// password: '',
-	};
-	const classes = useStyles();
+  const initialValues = {
+    surname: '',
+    matricNumber: '',
+  };
+  const classes = useStyles();
 
-	const handleSubmit = ({ email, matricNumber, /*password,*/ surname }) => {
-		history.push("/signup", { email, matricNumber, surname, /*password*/ })
-	};
+  const handleSubmit = ({ matricNumber, surname }) => {
+    history.push('/signin', { matricNumber, surname });
+  };
 
-	return (
-		<div className={classes.root}>
-			<Navbar />
-			<Container maxWidth="xl">
-				<Grid container spacing={4}>
-					<Grid item xs={12} className={classes.column}>
-						<div className={classes.formContainer}>
-							<h1 className={classes.heading}>Jump right back in</h1>
-							<p className={classes.textLight}>Sign in to continue</p>
-							<div className={classes.form}>
-								<Form
-									initialValues={initialValues}
-									validationSchema={validationSchema}
-									onSubmit={handleSubmit}
-								>
-									<FormField
-										name="email"
-										label="Email (School email)"
-										type="email"
-									/>
-									<FormField name="surname" label="Surname" type="password" />
-									<FormField name="matricNumber" label="Matriculation Number" />
-									{/* <FormField name="password" label="Password" type="password" /> */}
-									<SubmitButton>Sign in</SubmitButton>
-								</Form>
-							</div>
-						</div>
-					</Grid>
-					<Grid
-						item
-						xs={12}
-						className={`${classes.column} ${classes.onboarding}`}
-					>
-						<div>
-							<img width="100%" src={onboardingImage} alt="onboarding" />
-						</div>
-					</Grid>
-				</Grid>
-			</Container>
-		</div>
-	);
+  return (
+    <div className={classes.root}>
+      <Navbar />
+      <Container maxWidth="xl">
+        <Grid container spacing={4}>
+          <Grid item xs={12} className={classes.column}>
+            <div className={classes.formContainer}>
+              <h1 className={classes.heading}>Jump right back in</h1>
+              <p className={classes.textLight}>Sign in to continue</p>
+              <div className={classes.form}>
+                <Form
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={handleSubmit}
+                >
+                  <FormField name="matricNumber" label="Matriculation Number" />
+                  <FormField name="surname" label="Surname" type="password" />
+                  <SubmitButton>Sign in</SubmitButton>
+                </Form>
+              </div>
+            </div>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            className={`${classes.column} ${classes.onboarding}`}
+          >
+            <div>
+              <img width="100%" src={onboardingImage} alt="onboarding" />
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
